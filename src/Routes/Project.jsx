447c83af2db2e48711ae2437df9e31112b040b57
jiji-hoon96 <94469974+jiji-hoon96@users.noreply.github.com>
2022-06-05 16:00:27 +0900

    6-5 스킬 form 까지 구현완료

--- src/Routes/Project.jsx
+++ src/Routes/Project.jsx
@@ -1,10 +1,21 @@
+import styled from "styled-components";
 import Navbar from "../components/Navbar";
+
+const SkillListDiv = styled.div`
+  margin-top: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 32px;
+
+`
+
 function Project() {
   const skillList = localStorage.getItem("skill");
   return (
     <>
     <Navbar/>
-    <h1>{skillList}</h1>
+    <SkillListDiv>{`선택한 Skill : ${skillList}`}</SkillListDiv>
     </>
     
   )

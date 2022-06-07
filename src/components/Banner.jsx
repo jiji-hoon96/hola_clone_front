ac1ee3f915cf88dfb2c96cdaee2dfe6c243ac0ea
jiji-hoon96 <94469974+jiji-hoon96@users.noreply.github.com>
2022-06-07 15:09:37 +0900

    7-1 배너 수정 (1)

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,17 +1,42 @@
 import styled from 'styled-components'
-const BannerDiv = styled.div`
-    display: flex;
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
+const SliderDiv= styled.div`
+  margin: 0 auto;
+  margin-top: 20px;
+  margin-bottom: 40px;
+  width:80%;
+`
+
+const BannerDiv = styled.div`
+    position: relative;
+    text-align: center;
     width:100%;
     height:400px;
     background-image: linear-gradient(to right, #f78ca0 0%, #f9748f 19%, #fd868c 60%, #fe9a8b 100%);
 `
+const GitBannerDiv = styled.div`
+   position: relative;
+    width:100%;
+    height:400px;
+    text-align: center;
+    background-color: black;
+`
+
+const OkkyBannerDiv = styled.div`
+position: relative;
+    width:100%;
+    height:400px;
+    text-align: center;
+    background-image: linear-gradient(to right, #8cc3f7 0%, #7481f9 19%, #2734f3 60%, #0f00dd 100%);
+`
 
 const NoticeBtn = styled.button`
-    width:100px;
+    position: relative;
+    top:150px;
+    width:200px;
     height:50px;
     background-color: white;
     border: none;
@@ -25,24 +50,69 @@ const NoticeBtn = styled.button`
         background-color: #F74053;
     }
 `
+function NextArrow(props) {
+    const { className, style, onClick } = props;
+    return (
+      <div
+        className={className}
+        style={{ ...style, display: "block", background: "#fa798f"  }}
+        onClick={onClick}
+      />
+    );
+  }
 
-const NoticeH1= styled.div`
-    
-    width:500px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    border-radius: 10px;
-    height:100px;
-    font: 24px bolder;
-`
-
+function PrevArrow(props) {
+    const { className, style, onClick } = props;
+    return (
+      <div
+        className={className}
+        style={{ ...style, display: "block", background: "#fa798f" }}
+        onClick={onClick}
+      />
+    );
+  }
+  
 function Banner (){
+    const settings = {
+        dots: true,
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        speed: 2000,
+        autoplaySpeed: 3000,
+        cssEase: "linear",
+        nextArrow: <NextArrow />,
+        prevArrow: <PrevArrow />
+      }
     return(
-        <BannerDiv>
-            <NoticeBtn>Notion</NoticeBtn>
-            <NoticeH1>Organiztion에 대한 정보를 얻어보세요!</NoticeH1>
-        </BannerDiv>
+        <SliderDiv>
+        <Slider {...settings}>
+            <BannerDiv>
+                <NoticeBtn>
+                    <a href="https://rustic-tray-8dd.notion.site/0c464036142a430d97ace8b2e9217d13" target='_blank'> 
+                        Organization Notion   
+                    </a>
+                </NoticeBtn>
+            </BannerDiv>
+            <GitBannerDiv>
+                <NoticeBtn>
+                    <a href='https://github.com/h-dt' target="_blank">
+                        Organization Git
+                    </a>
+                </NoticeBtn>
+            </GitBannerDiv>
+            <OkkyBannerDiv>
+                <NoticeBtn>
+                    <a href='https://okky.kr/' target="_blank">
+                        Okky
+                    </a>
+                </NoticeBtn>
+            </OkkyBannerDiv>    
+            </Slider>
+        </SliderDiv>
+        
+        
     )
 }
 

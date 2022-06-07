3b1ebbdeea9d5fb2dbf7b71024f1c7bd918e57b9
jiji-hoon96 <94469974+jiji-hoon96@users.noreply.github.com>
2022-06-07 23:11:41 +0900

    9-1 로그인 창 수정완료

--- src/Routes/Login.jsx
+++ src/Routes/Login.jsx
@@ -12,7 +12,7 @@ const LoginForm = styled.div`
 
 const ModalForm = styled.div`
 width: 500px;
-height: 800px;
+height: 650px;
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -22,14 +22,45 @@ height: 800px;
   border-radius: 10px;
 `;
 
-const BtnDiv = styled.div`
-  width:250px;
+const ToggleDiv= styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
   height:100px;
+`
+const ToggleBtn = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  font-size: 14px;
+  width:200px;
+  height:50px;
+  margin: 10px;
+  cursor: pointer;
+  :hover{
+    transform: scale(1.1);
+  }
+  div{
+    width:170px;
+    height:10px;
+    margin-top: 5px;
+    background-color:#F9758F ;
+  }
+`
+
+const BtnDiv = styled.div`
+  border-radius: 10px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 10px 0px;
+  box-shadow: 2px 2px 2px 2px gray;
 `
 
 const GoogleBtn = styled.div`
   width: 200px;
-  height: 70px;
+  height: 66px;
   background-color: transparent;
   background-image: url("img/logofile/googleimage.png");
   background-position: center;
@@ -43,8 +74,8 @@ const GoogleBtn = styled.div`
 `;
 
 const GithubBtn = styled.div`
-  width: 200px;
-  height: 70px;
+  width: 130px;
+  height: 130px;
   border-radius: 20px;
   background-image: url("img/logofile/gitimage.png");
   background-position: center;
@@ -59,9 +90,9 @@ const GithubBtn = styled.div`
   }
 `;
 const KaKaoBtn = styled.div`
-  width: 200px;
-  height: 70px;
-  background-image: url("https://i0.wp.com/wjclinic.com/wp-content/uploads/2020/08/png-clipart-kakao-talk-logo-kakaotalk-apple-kakaostory-kakaostyle-kakao-talk-text-computer.png?ssl=1");
+  width: 140px;
+  height: 140px;
+  background-image: url("https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2Fem7IrN%2Fbtq7snvbsK4%2FEKApcyLKGqBGWr8tmsbkXK%2Fimg.png");
   background-position: center;
   background-repeat: none;
   background-size: cover;
@@ -79,7 +110,7 @@ const Form = styled.form`
   display: flex;
   font-weight: 100;
   justify-content: center;
-  height: 300px;
+  height: 500px;
   align-items: center;
   flex-direction: column;
   input {
@@ -87,7 +118,7 @@ const Form = styled.form`
     cursor: pointer;
     width: 250px;
     height: 50px;
-    margin: 5px;
+    margin: 10px;
     border: 1px solid "#fd8f8c";
     color: "#fd8f8c";
     border-radius: 10px;
@@ -95,10 +126,10 @@ const Form = styled.form`
     font-weight: 100;
     color: "#fd8f8c";
     :focus {
-      color: "#fd8f8c";
+      
       font-weight: bolder;
       transform: scale(1.09);
-      background-color: "#fd8f8c";
+      
     }
   }
   span {
@@ -106,10 +137,9 @@ const Form = styled.form`
   }
 `;
 const SubmitBtn = styled.input`
-  color:  "#fd8f8c";
   :hover{
     transform: scale(1.1);
-    background-color:#fd8f8c;
+    
   }
 `
 const LoginImg = styled.div`
@@ -141,9 +171,13 @@ function Login() {
     formState: { errors },
   } = useForm({mode:"onChange"});
   const [login, setLogin] = useState("")
+  const [logintoggle,setLoginToggle] =useState(false);
   const onSubmitValid=(data)=>{
     setLogin(data)
     reset()
+  }
+  const onToggle = (boolean) =>{
+    setLoginToggle(boolean)
   }
   return (
     <>
@@ -151,19 +185,32 @@ function Login() {
       <LoginTitle>Organiztion에 오신 것을 환영합니다!</LoginTitle>
         <LoginForm>
         <ModalForm>
-          <BtnDiv>
-            <GoogleBtn/>
-          </BtnDiv>
-          <h1>Google 로그인</h1>  
-          <BtnDiv>
-            <GithubBtn/>
-          </BtnDiv>
-          <h1>Github 로그인</h1>        
-          <BtnDiv>
-            <KaKaoBtn/>
-          </BtnDiv>
-          <h1>KaKao 로그인</h1>        
-          <LoginImg/>
+          <ToggleDiv>
+            <ToggleBtn onClick={()=>onToggle(true)}>
+              소셜 계정으로 로그인하기
+              {logintoggle ? <div/> : null}
+            </ToggleBtn>
+            <ToggleBtn onClick={()=>onToggle(false)}>
+              ID/PW 으로 로그인하기
+              {logintoggle ? null:  <div/> }
+            </ToggleBtn>
+          </ToggleDiv>
+          {logintoggle ? 
+          <>
+            <BtnDiv style={{backgroundColor:"white"}}>
+              <GoogleBtn/>
+            </BtnDiv>
+            <h1>Google 로그인</h1>  
+            <BtnDiv style={{backgroundColor:"#1B1F23"}}>
+              <GithubBtn/>
+            </BtnDiv>
+            <h1>Github 로그인</h1>        
+            <BtnDiv  style={{backgroundColor:"#FAE300"}}>
+              <KaKaoBtn/>
+            </BtnDiv>
+            <h1>KaKao 로그인</h1>        
+          </> : 
+          <>
           <Form onSubmit={handleSubmit(onSubmitValid)}>
               <input
               {...register("username", {
@@ -183,6 +230,9 @@ function Login() {
               <span>{errors.password?.message}</span>
               <SubmitBtn type="submit" value="로그인" style={{ fontWeight: "bolder" }}/>
           </Form>
+          </>}
+         
+  
         </ModalForm>
         </LoginForm>
     </>

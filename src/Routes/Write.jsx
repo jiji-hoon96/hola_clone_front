a4a6e6bfd8dac11ad246a1dd1fe3371e4307d75f
jiji-hoon96 <94469974+jiji-hoon96@users.noreply.github.com>
2022-06-09 21:04:21 +0900

    12-3 게시판 구현 중 에러 해결
    Failed to parse source map (@toast-ui/editor/dist/purify.js.map)
    이라고 애러가 뜸
    
    해결방안 =>
    "start" : "set \"GENERATE_SOURCEMAP=false\" && react-scripts start",
    로 해결

--- src/Routes/Write.jsx
+++ src/Routes/Write.jsx
@@ -11,7 +11,7 @@ import '@toast-ui/editor/dist/toastui-editor.css';
 import { Editor } from '@toast-ui/react-editor';
 import colorSyntax from '@toast-ui/editor-plugin-color-syntax';
 import "tui-color-picker/dist/tui-color-picker.css";
-import "@toast-ui/editor-plugin-color-syntax/dist/toastui-editor-plugin-color-syntax.css"
+import "@toast-ui/editor-plugin-color-syntax/dist/toastui-editor-plugin-color-syntax.css";
 import "@toast-ui/editor/dist/i18n/ko-kr";
 
 
@@ -231,6 +231,7 @@ function Write() {
             </Select>
           </FormControl>
           <Editor
+            usageStatistics= {false}
             initialValue="안녕하세요 React Editor입니다"
             previewStyle="vertical"
             height="600px"
